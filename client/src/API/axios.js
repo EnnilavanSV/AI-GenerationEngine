f76068fa02bd1fe19e651a8d2998cfc6e27492b4
EnnilavanSV <ennilavan.contact@gmail.com>
2026-06-08 23:59:34 +0530

    Delete client/src/API directory

--- client/src/API/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Create a custom Axios instance pointing to our Node backend
-const API = axios.create({
-  baseURL: "https://ai-generation-engine.onrender.com/api",
-});
-
-export default API;
