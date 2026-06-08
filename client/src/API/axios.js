2d0a8ae40c15996d2c83abb0e9f4b51668f8667e
Ennilavan <ennilavan13@gmail.com>
2026-06-08 23:47:53 +0530

    Updated API link for production

--- client/src/API/axios.js
+++ client/src/API/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create a custom Axios instance pointing to our Node backend
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: "https://ai-generation-engine.onrender.com/api",
 });
 
 export default API;
